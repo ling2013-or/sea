463b391482114205490e64b17370197ea263ee4b
jj <sxliujianjian@163.com>
2016-09-06 00:34:27 +0800

    压缩文件删除

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2016/8/7
- * Time: 17:06
- */
-//readfile('./web/html/view/index.html');
-header('location:./web/html/view/index.html');
